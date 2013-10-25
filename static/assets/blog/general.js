ed6a4ac016f571394dad68309c89536cea1610b4
Vladimir Smirnov <mail@mindcollapse.com>
2013-10-25 10:10:26 +0300

    few more improvements

--- static/assets/blog/general.js
+++ static/assets/blog/general.js
@@ -25,5 +25,26 @@ $(function(){
 			e.originalEvent.clipboardData.setData("Text", selected_text);
 		}
 	})
+
+	// keyboard navigation
+	$(window).on("keyup", function(e){
+		if (e.altKey) {
+			var newloc = null;
+
+			console.log(e);
+
+			if (e.which == 37) {
+				newloc = $("link[rel*=prev]").attr("href");
+			} else if (e.which == 39) {
+				newloc = $("link[rel*=next]").attr("href");
+			} else if (e.which == 38) {
+				newloc = $("link[rel*=up]").attr("href");
+			} 
+
+			if (newloc) {
+				document.location.href = newloc;
+			}
+		}
+	})
 })
 
